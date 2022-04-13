08037146d9f0e0aba290a068945c13c41c3e0981
Szymon Murawski <szymon.murawski@gmail.com>
2022-04-13 22:54:26 +0200

    feat: DVDRentalStoreConsoleApp in .NET 3.0

---
+++ NetCore30/DVDRentalStoreConsoleApp/DVDRentalStoreConsoleApp/ConsoleMenu.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DVDRentalStoreConsoleApp
+{
+    class ConsoleMenu
+    {
+        public int CurrentElement { get; set; }
+        private ConsoleMenuElement[] MenuElements { get; set; }
+        public ConsoleMenu(ConsoleMenuElement[] menuElements)
+        {
+            CurrentElement = 0;
+            MenuElements = menuElements;
+        }
+        public void PrintMenu()
+        {
+            for (int i = 0; i < MenuElements.Length; i++)
+            {
+                if (i == CurrentElement)
+                {
+                    Console.BackgroundColor = ConsoleColor.Gray;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                }
+                Console.WriteLine(MenuElements[i]);
+                Console.ResetColor();
+            }
+        }
+        public void RunMenu()
+        {
+            while (true)
+            {
+                PrintMenu();
+                ConsoleKeyInfo pressedKey = Console.ReadKey();
+                switch (pressedKey.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        CurrentElement = (CurrentElement - 1 + MenuElements.Length) % MenuElements.Length;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        CurrentElement = (CurrentElement + 1) % MenuElements.Length;
+                        break;
+                    case ConsoleKey.Enter:
+                        MenuElements[CurrentElement].ActionToRun();
+                        Console.WriteLine("Press enter to continue");
+                        Console.ReadLine();
+                        break;
+                }
+                Console.Clear();
+            }
+        }
+    }
+}
